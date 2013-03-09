32d08fb573283187d736b5d1999be07879c80f7b
Eric <ekrause@Erics-MacBook.local>
2013-03-09 14:35:30 -0800

    better go

--- go.sh
+++ go.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-echo Running DIST-FP-1
+echo Old DIST-FP-1
+cat orig/fp.txt
+echo New:
 ./predictor traces/DIST-FP-1
-echo Running DIST-INT-1
+
+echo Old DIST-INT-1
+cat orig/int.txt
+echo New:
 ./predictor traces/DIST-INT-1
-echo Running DIST-MM-1
+
+echo Old DIST-MM-1
+cat orig/mm.txt
+echo New:
 ./predictor traces/DIST-MM-1
-echo Running DIST-SERV-1
+
+echo Old DIST-SERV-1
+cat orig/serv.txt
+echo New:
 ./predictor traces/DIST-SERV-1
 printf '\a'
