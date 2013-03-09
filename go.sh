70425078863d42c3f4aca4ea0fe7584a8ed808c3
Eric <ekrause@Erics-MacBook.local>
2013-03-09 14:49:36 -0800

    hey the script isn't shit!

--- go.sh
+++ go.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
-echo Old DIST-FP-1
 cat orig/fp.txt
-echo \nNew:
+echo -e "\nNew:"
 ./predictor traces/DIST-FP-1
 
-echo \nOld DIST-INT-1
 cat orig/int.txt
-echo \nNew:
+echo -e "\n"New:
 ./predictor traces/DIST-INT-1
 
-echo \nOld DIST-MM-1
 cat orig/mm.txt
-echo \nNew:
+echo -e "\n"New:
 ./predictor traces/DIST-MM-1
 
-echo \nOld DIST-SERV-1
 cat orig/serv.txt
-echo \nNew:
+echo -e "\n"New:
 ./predictor traces/DIST-SERV-1
 printf '\a'
