f9fec690589b6d766aaf781853dac716c5a6415b
Eric <ekrause@Erics-MacBook.local>
2013-03-15 20:25:09 -0700

    configurable cache sort of works

--- predictor.cc
+++ predictor.cc
@@ -2,6 +2,7 @@
 #define PC (br->instruction_addr)
 #define PC10 (keep_lower(br->instruction_addr,10))
 #define NEXT (br->instruction_next_addr)
+#define PC_LOWER (keep_lower(PC,INDEX))
 
 PREDICTOR::PREDICTOR()
 {
@@ -17,72 +18,60 @@ PREDICTOR::PREDICTOR()
     alpha_choice[i] = 2;
   }
   phistory = 0;
-
 }
 
-bool PREDICTOR::get_local_predict(const branch_record_c* br, uint *predicted_target_address)
-{
-  uint16_t alpha_lht_ind = PC10;
-  uint16_t curr = alpha_lht[alpha_lht_ind];
-  uint16_t pred_bits = alpha_lpt[curr];
+bool PREDICTOR::get_local_predict(const branch_record_c* br)
+  {
+    uint16_t alpha_lht_ind = PC10;
+    uint16_t curr = alpha_lht[alpha_lht_ind];
+    uint16_t pred_bits = alpha_lpt[curr];
 
-  return (pred_bits & 4)>>2;
-}
-
-bool PREDICTOR::get_global_predict(const branch_record_c* br, uint *predicted_target_address)
-{
+    return (pred_bits & 4)>>2;
+  }
+bool PREDICTOR::get_global_predict(const branch_record_c* br)
+  {
   return (alpha_gpt[phistory]&2)>>1;
-}
-
+  }
 bool PREDICTOR::get_prediction(const branch_record_c* br, const op_state_c* os, uint *predicted_target_address)
 {
   //  ALPHA PREDICTION
-  local_prediction = get_local_predict(br, predicted_target_address);
-  global_prediction = get_global_predict(br, predicted_target_address);
-  pred_choice = choose_predictor(br);
-  final_prediction = (pred_choice == PRED_LOCAL) ? local_prediction : global_prediction;
+    local_prediction = get_local_predict(br);
+    global_prediction = get_global_predict(br);
+    pred_choice = choose_predictor(br);
 
-  //  TARGET PREDICTION
+    final_prediction = (pred_choice == PRED_LOCAL) ? local_prediction : global_prediction;
 
-  return final_prediction;   // true for taken, false for not taken
+//  TARGET PREDICTION
+  if( thecache.predict(br, predicted_target_address) == false );
+    thecache.needs_update = true;
+return final_prediction;
 }
 
-// Update the predictor after a prediction has been made.  This should accept
-// the branch record (br) and architectural state (os), as well as a third
-// argument (taken) indicating whether or not the branch was taken.
 void PREDICTOR::update_predictor(const branch_record_c* br, const op_state_c* os, bool taken, uint actual_target_address)
 {
-  // ALPHA PREDICTOR UPDATE
   uint16_t curr = alpha_lht[PC10];
-
+// ALPHA PREDICTOR UPDATE
   // update prediction tables
   if (taken)
   {
-    if (alpha_lpt[curr] < 7)
-      alpha_lpt[curr]++;
-    if (alpha_gpt[phistory] < 3)
-      alpha_gpt[phistory]++;
+    if (alpha_lpt[curr] < 7)      alpha_lpt[curr]++;
+    if (alpha_gpt[phistory] < 3)  alpha_gpt[phistory]++;
   }
   else
   {
-    if (alpha_lpt[curr] > 0)
-      alpha_lpt[curr]--;
-    if (alpha_gpt[phistory] > 0)
-      alpha_gpt[phistory]--;
+    if (alpha_lpt[curr] > 0)      alpha_lpt[curr]--;
+    if (alpha_gpt[phistory] > 0)  alpha_gpt[phistory]--;
   }
-
   // update prediction choice table
   if (taken != final_prediction && global_prediction != local_prediction) // misprediction occured, but one predictor was correct
   {
     if (taken == global_prediction)
     {
-      if (alpha_choice[phistory] < 3)
-        alpha_choice[phistory]++;
+      if (alpha_choice[phistory] < 3)   alpha_choice[phistory]++;
     }
     else
     {
-      if (alpha_choice[phistory] > 0)
-        alpha_choice[phistory]--;
+      if (alpha_choice[phistory] > 0)   alpha_choice[phistory]--;
     }
   }
 
@@ -91,6 +80,13 @@ void PREDICTOR::update_predictor(const branch_record_c* br, const op_state_c* os
 
   // update local history table:  shift left, OR with taken, keep 10 bits
   alpha_lht[PC10] = keep_lower((alpha_lht[PC10] << 1) | taken, 10);
+
+// TARGET UPDATE
+  if( thecache.needs_update )
+  {
+    thecache.update(br, actual_target_address);
+    thecache.needs_update = false;
+  }
 
   return;
 }
@@ -105,11 +101,60 @@ bool PREDICTOR::choose_predictor(const branch_record_c* br)
   curr_alpha_choice_entry = ( curr_alpha_choice_entry & 2 );      // only care about bit 1 of saturating counter
 
   if (curr_alpha_choice_entry == 0)  return PRED_LOCAL;  // bit 1 was not set
-  else                      return PRED_GLOBAL; // bit 1 was set
+  else return PRED_GLOBAL; // bit 1 was set
+}
+
+CACHE::CACHE()
+{
+  needs_update = false;
+
+}
+
+// returns true on hits (and updates *predicted_target_address )
+// returns false on misses (and sets *predicted_target_address to 0)
+bool CACHE::predict(const branch_record_c* br, uint *predicted_target_address)
+{
+  for (int i = 0; i < WAYS; i++)
+  {
+    if( tag[PC_LOWER][i] == PC >> INDEX )
+    {
+      *predicted_target_address = data[PC_LOWER][i];
+      return true;
+    }
+  }
+
+  // cycled through all ways and no tags matched.  Not in cache.o
+  *predicted_target_address = 0;
+  return false;
+}
+
+// no need to call unless misprediction or empty
+void CACHE::update(const branch_record_c* br, uint actual_target_address)
+{
+  // if there is an empty way, put the data there
+  for (int i = 0; i < WAYS; i++)
+  {
+    if (data[PC_LOWER][i] == 0) // look through all ways
+      tag[PC_LOWER][i] = PC >> INDEX;  // empty tag field found, indicating empty way
+      data[PC_LOWER][i] = actual_target_address; // store branch address in empty way
+      return;
+  }
+
+  //else, we need to evict
+  uint victim = count[PC_LOWER];  // read out the current victim
+  tag[PC_LOWER][victim] = PC >> INDEX;  // use overwrite victim's tag field
+  data[PC_LOWER][victim] = actual_target_address; // overwrite victim's data field
+  count[PC_LOWER] = (victim++) % WAYS;    // point to next victim
+  return;
+
+
+  //tag[count[PC_LOWER]][PC_LOWER] = actual_target_address >> INDEX;
+  //count[PC_LOWER] = (count[PC_LOWER]++) % WAYS;
+  //return;
 }
 
 // keeps lower n_bits of target ( glorified AND operation, because C++ != verilog )
-uint32_t PREDICTOR::keep_lower(uint32_t target, int n_bits)
+uint32_t keep_lower(uint32_t target, int n_bits)
 {
     return target & ((1<<n_bits)-1);
 }
--- predictor.h
+++ predictor.h
@@ -23,6 +23,26 @@
 #define TAKEN true
 #define NOT_TAKEN false
 
+#define ENTRIES 64
+#define INDEX 6
+#define WAYS 8
+#define WAYSBITS 3
+
+
+class CACHE
+{
+public:
+  CACHE();
+
+  uint data[ENTRIES][WAYS];
+  uint tag[ENTRIES][WAYS];
+  uint count[ENTRIES];
+  bool needs_update;
+
+  bool predict(const branch_record_c* br, uint *predicted_target_address);
+  void update(const branch_record_c* br, uint actual_target_address);
+};
+
 class PREDICTOR
 {
 public:
@@ -32,34 +52,30 @@ public:
   void update_predictor(const branch_record_c* br, const op_state_c* os, bool taken, uint actual_target_address);
   int btb_mispredicts;
   bool btb_used;
-
 private:
 
-  uint16_t alpha_lht[SIZE_1K];  //  local history table (1024*10)/8     = 1280 bytes
-  uint16_t alpha_lpt[SIZE_1K];  //  local prediction table (1024*3)/8   = 384 bytes
-
-  uint16_t alpha_gpt[SIZE_4K];  //  global predition table (4096*2)/8   = 1024 bytes
-
-  uint16_t alpha_choice[SIZE_4K];  //  choice predition table (4096*2)/8   = 1024 bytes
-
-                          //                                total = 3713.5 bytes = 3712 bytes
+  // ALPHA STUFF
+  uint16_t alpha_lht[SIZE_1K];  //  local history table (1024*10)/8         = 1280 bytes
+  uint16_t alpha_lpt[SIZE_1K];  //  local prediction table (1024*3)/8       = 384 bytes
+  uint16_t alpha_gpt[SIZE_4K];  //  global predition table (4096*2)/8       = 1024 bytes
+  uint16_t alpha_choice[SIZE_4K];  //  choice predition table (4096*2)/8    = 1024 bytes
+                          //                                          total = 3712 bytes
   uint32_t phistory;
   bool pred_choice;
   bool local_prediction;
   bool global_prediction;
   bool final_prediction;
-
-  bool get_local_predict(const branch_record_c* br, uint *predicted_target_address);
-
-  bool get_global_predict(const branch_record_c* br, uint *predicted_target_address);
+  bool get_local_predict(const branch_record_c* br);
+  bool get_global_predict(const branch_record_c* br);
 
   // returns PRED_LOCAL or PRED GLOBAL indicating whether local or global history should be used
+
   bool choose_predictor(const branch_record_c* br);
-
-  // keeps lower n_bits of target
-  uint32_t keep_lower(uint32_t target, int n_bits);
-
+  // TARGET STUFF
+  CACHE thecache;
 };
 
-#endif // PREDICTOR_H_SEEN
+// keeps lower n_bits of target
+uint32_t keep_lower(uint32_t target, int n_bits);
 
+#endif // PREDICTOR_H_SEEN
